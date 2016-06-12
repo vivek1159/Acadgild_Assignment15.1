397dea8b163fcb06ef4ae334e572a5f86e2dce4b
vivek1159 <vivek1159@gmail.com>
2016-06-12 20:32:34 +0530

    Initial upload.

---
+++ app/src/main/java/com/example/vivek/assignment151/MyService.java
@@ -0,0 +1,49 @@
+package com.example.vivek.assignment151;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class MyService extends Service {
+
+    MediaPlayer mp;
+
+    public MyService() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp = MediaPlayer.create(this,R.raw.song);
+        mp.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.stop();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+}
